2160faa848271261d6e5d29e9d7b1d4d77db2883
shemaaali <shemaali8787@gmail.com>
2020-09-01 13:54:12 -0500

    added more changes as states abbrev

--- lambdata/myfun.py
+++ lambdata/myfun.py
@@ -1,24 +1,18 @@
 def largenum(x):
 
-    #"""
-    #x is a large number
+    """
+    x is a large number
+    
+    function will be a large number (that can be interager or float)
 
-    #"""
+    """
 
-    #return x * 200
+    return x * 200
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
 
        print("Hello Python")
        y = int(input("Enter a number"))
        print(y, largenum(y))
-
-def func_return(a, b):
-    return a + b, a * b, a / b
-
-Z = func_return(12, 6)
-
-print(Z)
-print(type(Z))
 
 
